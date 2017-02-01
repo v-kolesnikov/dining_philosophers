972c179fb28631dbce7bccd41c80ab3c5d9cd117
Vasily Kolesnikov <re.vkolesnikov@gmail.com>
2017-02-01 23:05:20 +0300

    Add DiningPhilosophers module

---
+++ lib/dining_philosophers.rb
@@ -0,0 +1,49 @@
+require 'thread'
+
+module DiningPhilosophers
+  class Chockstick
+    attr_reader :id, :mutex
+
+    def initialize(id)
+      @id = id
+      @mutex = Mutex.new
+    end
+
+    def lock(name)
+      mutex.synchronize do
+        puts "#{name} lock chockstick ##{id}"
+        yield if block_given?
+        puts "#{name} unlock chockstick ##{id}"
+      end
+    end
+  end
+
+  class Philosopher
+    attr_reader :name, :left_chopstick, :right_chopstick
+
+    def initialize(name, left_chopstick, right_chopstick)
+      @name = name
+      @left_chopstick = left_chopstick
+      @right_chopstick = right_chopstick
+    end
+
+    def run
+      loop do
+        think
+        left_chopstick.lock(name) do
+          right_chopstick.lock(name) { eat }
+        end
+      end
+    end
+
+    def think
+      puts "#{name} thinking..."
+      sleep(rand(2))
+    end
+
+    def eat
+      puts "#{name} eating..."
+      sleep(rand(2))
+    end
+  end
+end
---
+++ spec/dining_philosophers_spec.rb
@@ -0,0 +1,47 @@
+require "spec_helper"
+
+RSpec.describe DiningPhilosophers do
+  let(:chocksticks) do
+    (1..5).map do |number|
+      DiningPhilosophers::Chockstick.new(number)
+    end
+  end
+
+  let(:philosophers) do
+    names = %w(Пифагор Сократ Платон Диоген Аристотель)
+    (0..4)
+      .to_a
+      .concat([0])
+      .each_cons(2)
+      .zip(names)
+      .map do |(left, right), name|
+        DiningPhilosophers::Philosopher
+          .new(name, chocksticks[left], chocksticks[right])
+      end
+  end
+
+  it "setup" do
+    expect(chocksticks.count).to eq 5
+
+    expect(philosophers[0].left_chopstick.id).to eq 1
+    expect(philosophers[0].right_chopstick.id).to eq 2
+
+    expect(philosophers[1].left_chopstick.id).to eq 2
+    expect(philosophers[1].right_chopstick.id).to eq 3
+
+    expect(philosophers[2].left_chopstick.id).to eq 3
+    expect(philosophers[2].right_chopstick.id).to eq 4
+
+    expect(philosophers[3].left_chopstick.id).to eq 4
+    expect(philosophers[3].right_chopstick.id).to eq 5
+
+    expect(philosophers[4].left_chopstick.id).to eq 5
+    expect(philosophers[4].right_chopstick.id).to eq 1
+  end
+
+  it 'run' do
+    philosophers.map do |philosopher|
+      Thread.new { philosopher.run }
+    end.map(&:join)
+  end
+end
